3b560d2a9873f6dea636844bf96c2c5571ef7ce6
tolstoy92 <33932807+tolstoy92@users.noreply.github.com>
2020-02-11 11:14:23 +0300

    Update download_model.sh
    models links

--- tools/download_model.sh
+++ tools/download_model.sh
@@ -3,5 +3,5 @@
 cd ..
 mkdir model
 cd model
-wget https://download.01.org/opencv/2019/open_model_zoo/R1/2019/road-segmentation-adas-0001/FP16/road-segmentation-adas-0001.bin
-wget https://download.01.org/opencv/2019/open_model_zoo/R1/2019/road-segmentation-adas-0001/FP16/road-segmentation-adas-0001.xml
+wget https://download.01.org/opencv/2019/open_model_zoo/R1/20190404_140900_models_bin/road-segmentation-adas-0001/FP16/road-segmentation-adas-0001.bin
+wget https://download.01.org/opencv/2019/open_model_zoo/R1/20190404_140900_models_bin/road-segmentation-adas-0001/FP16/road-segmentation-adas-0001.xml
